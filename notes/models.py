7968c9f1fc5b37d4414621f24735673a336eb415
Blaq Ice <blaqice0@gmail.com>
2022-04-23 10:47:23 +0000

    Allowed blank on text field

--- notes/models.py
+++ notes/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Note(models.Model):
     title = models.CharField(max_length=155)
-    text = models.TextField(max_length=5000)
+    text = models.TextField(max_length=5000, blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
