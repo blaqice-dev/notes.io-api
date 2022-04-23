7a748a2dafac851ff7f184b1d289577238eafaec
Blaq Ice <blaqice0@gmail.com>
2022-04-23 11:16:19 +0000

    Fix note urls

--- notesapi/urls.py
+++ notesapi/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/notes/', include('notes.api'))
+    path('api/v1/notes/', include('notes.urls'))
 ]
