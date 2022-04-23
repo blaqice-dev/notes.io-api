6070b7e1d4005b46a41b3d347a1d928c09c67a47
Blaq Ice <blaqice0@gmail.com>
2022-04-23 11:32:34 +0000

    Fix auto updating created datetime

--- notes/models.py
+++ notes/models.py
@@ -4,11 +4,5 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=155)
     text = models.TextField(max_length=5000, blank=True, null=True)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=False, null=True, blank=True)
-
-    def save(self, *args, **kwargs):
-        if self.created:
-            pass
-        else:
-            super(Note, self).save(*args, **kwargs)
