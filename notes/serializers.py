bf1d79a69cda4519f9b694bc00b6e5e53696a843
Blaq Ice <blaqice0@gmail.com>
2022-04-23 10:47:50 +0000

    Added NoteSerializer

---
+++ notes/serializers.py
@@ -0,0 +1,23 @@
+from django.utils import timezone
+from rest_framework import  serializers
+
+from .models import Note
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    text = serializers.CharField(allow_null=True, allow_blank=True)
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'text', 'created', 'updated')
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.text = validated_data.get('text', instance.text)
+
+        updated = timezone.now()
+        instance.updated = updated
+        instance.save()
+        return instance
