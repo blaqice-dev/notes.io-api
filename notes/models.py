1b7236d379c952393628e2aa162b9eae2d8586f0
Blaq Ice <blaqice0@gmail.com>
2022-04-23 10:39:57 +0000

    Added Notes model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=155)
+    text = models.TextField(max_length=5000)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
