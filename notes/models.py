e707381e6e455fdd3a1c5582f603054094bda060
Blaq Ice <blaqice0@gmail.com>
2022-04-23 11:15:58 +0000

    Fix auto datetime on update

--- notes/models.py
+++ notes/models.py
@@ -5,4 +5,4 @@ class Note(models.Model):
     title = models.CharField(max_length=155)
     text = models.TextField(max_length=5000, blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=False)
